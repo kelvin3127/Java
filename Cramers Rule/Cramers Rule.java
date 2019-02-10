e2903c85301669b2443892b430d6980250a8c340
kelvin3127 <46061373+kelvin3127@users.noreply.github.com>
2019-02-09 23:16:12 -0500

    Add files via upload
    Simple Java code following the cramer's rule methods

---
+++ Cramers Rule/Cramers Rule.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+
+
+public class task1
+
+{
+	
+	public static void main(String[] args)
+	
+	{
+		
+		Scanner input = new Scanner(System.in);
+		//Create new Scanner object
+
+		
+		//Enter the values for the letters
+		
+		System.out.print("Enter the values for the following letters a, b, c, d, e and f in that order: ");
+
+		double a = input.nextDouble();
+		
+		double b = input.nextDouble();
+		
+		double c = input.nextDouble();
+		
+		double d = input.nextDouble();
+		
+		double e = input.nextDouble();
+		
+		double f = input.nextDouble();
+
+	
+	
+		double x = ((e*d) - (b*f)) / ((a*d) - (b*c)); 	
+		double y = ((a*f) - (e*c)) / ((a*d) - (b*c));	
+		if ( ((a*d) - (b*c)) == 0)
+		
+		{
+			
+			System.out.println("The equation has no solution");
+		
+		}
+		
+		else
+		
+		{
+			
+			System.out.println("x is " + x + " and y is " + y);
+
+		}
+
+	}
+
+}
+
+
+

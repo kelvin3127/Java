5fa92545969820611fbfce1515cf06269836fcda
kelvin3127 <46061373+kelvin3127@users.noreply.github.com>
2019-02-10 19:16:55 -0500

    two dimensional Arrays
    A quick two dimensional array snippit

---
+++ Location/Location 2.java
@@ -0,0 +1,26 @@
+
+public class Location
+{
+	
+	double row;
+	double column;
+	double maxValue;
+	
+	//Constructor
+	public Location()
+	{
+		row = 0;
+		column = 0;
+		 maxValue = 0;
+	}
+	
+	//Overloaded Constructor
+	public Location(double r, double c, double mv)
+	{
+		row = r;
+		column = c;
+		maxValue = mv;
+	}
+	
+	
+}
---
+++ Location/Location.java
@@ -0,0 +1,67 @@
+
+import java.util.Scanner;
+
+public class Task5 
+{
+	public static void main(String[] args) 
+	{
+	    // Create a Scanner
+	    Scanner input = new Scanner(System.in);
+	    
+	    //Display message and call for rows and columns
+	    System.out.println("Enter the number of rows in the array: ");
+	    int rows = input.nextInt();
+	    System.out.println("Enter the number of columns in the array: ");
+	    int columns = input.nextInt();
+	    
+	    //Create the array with specified size
+	    double[][] array = new double[rows][columns];
+	    
+	    //Display message and enter array data
+	    System.out.println("Enter the array: ");
+	    
+	    //Loop to enter the array data
+	    for (int i = 0; i < array.length; i++)
+	    {	
+	        for (int j = 0; j < array[i].length; j++)
+	        
+	          array[i][j] = input.nextDouble();
+	    }
+	    
+	    //make the Location object
+	    Location largeLocation = locateLargest(array);
+	    
+	    System.out.println("The location of the largest element is"
+	    + largeLocation + " at (" + largeLocation.row + ", " + largeLocation.column + ")");
+	    
+	    input.close();
+	}
+	
+	//Get the largest array
+	public static Location locateLargest(double[][] a)
+	{
+		Location tempLocation = new Location();
+		double maxVal = a[a.length][a.length-1];
+		
+		//Loop for the max value
+		for(int i = 0; i < a.length; i++)
+		{
+			for (int j = 0; j < a[i].length; j++)
+			{
+				
+				if (a[i][j] > maxVal)
+				{
+					a[i][j] = maxVal;
+
+				}
+			}
+		}
+		tempLocation.maxValue = maxVal;
+		return tempLocation;
+		
+	
+	}
+
+	
+	
+}
